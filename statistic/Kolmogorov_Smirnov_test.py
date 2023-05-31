c5ea0247e806243864e2feb2d3098d1c2e1ac01e
Akeann <108687819+Akeann@users.noreply.github.com>
2023-05-31 16:47:14 +0300

    Update Kolmogorov_Smirnov_test.py

--- statistic/Kolmogorov_Smirnov_test.py
+++ statistic/Kolmogorov_Smirnov_test.py
@@ -2,14 +2,9 @@ import pandas
 import matplotlib.pyplot as plt
 from scipy import stats
 
-df1 = pandas.read_csv("resistance.csv")
+df = pandas.read_csv("resistance.csv")
 # Замеры сопротивления набора резисторов с одинаковым номинальным сопротивлением.
-df = pandas.DataFrame(data={
-    'df1': df1['resistance'],
-})
 
 df.plot.kde()
 plt.show()
-print(stats.kstest(df, 'norm', (df.mean(), df.std()), N=5000))
-
-# Распределение крайне похоже на нормальное
+print(stats.kstest(df['resistance'], 'norm', (df['resistance'].mean(), df['resistance'].std()), N=5000))
